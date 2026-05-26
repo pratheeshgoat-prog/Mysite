b7c98eefbcf65dede14e43ab3cbcb3ecbe6145fc
pratheeshgoat-prog <pratheeshgoat@gmail.com>
2026-05-26 15:57:13 +0530

    Update track.js

--- api/track.js
+++ api/track.js
@@ -8,9 +8,7 @@ export default async function handler(req, res) {
     `https://ipinfo.io/${ip}/json`
   ).then(r => r.json());
 
-  res.status(200).json({
-    ip,
-    city: geo.city,
+  res.status(200).send("Hello");
     region: geo.region,
     country: geo.country,
     isp: geo.org
